7ff4c0b8f846bad6d8abe1daf64b2dd8117c628c
Cameron Norfleet <cameron@Camerons-MBP.lan>
2023-08-17 17:00:39 -0700

    1

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const envVar = process.env.NODE_ENV
+
 // Your code here
 
+//To set ENV Variable => (in terminal) NODE_ENV=production node index.js
 app.get('/', (req, res) => {
     res.send(process.env.SECRET_MESSAGE);
 });
